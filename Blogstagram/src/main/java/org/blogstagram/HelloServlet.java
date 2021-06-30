5ffd502c8168071f16cdd16bbb2ceff129f0cc11
nika-rgb <nchek19@freeuni.edu.ge>
2021-06-30 15:09:34 +0400

    initial

--- Blogstagram/src/main/java/org/blogstagram/HelloServlet.java
+++ Blogstagram/src/main/java/org/blogstagram/HelloServlet.java
@@ -5,11 +5,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
 
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/hello.jsp").forward(request,response);
+
     }
 
 }
