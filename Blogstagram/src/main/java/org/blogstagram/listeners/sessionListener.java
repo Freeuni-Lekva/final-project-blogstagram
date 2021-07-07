d2e46bd076e91894f3b9a3dd887cac32b8144200
auttenbergo <gigi.gvaramia@gmail.com>
2021-07-07 17:22:12 +0400

    Created connection in sessionListener

--- Blogstagram/src/main/java/org/blogstagram/listeners/sessionListener.java
+++ Blogstagram/src/main/java/org/blogstagram/listeners/sessionListener.java
@@ -1,14 +1,29 @@
 package org.blogstagram.listeners;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebListener
 public class sessionListener implements HttpSessionListener {
     @Override
     public void sessionCreated(HttpSessionEvent httpSessionEvent) {
         // add Dao objects
+        BasicDataSource source = new BasicDataSource();
+        source.setUsername("root");
+        source.setUrl("jdbc:mysql://localhost:3306/blogstagramdb");
+        try {
+            Connection dbConnection = source.getConnection();
+            /*
+                Add DAO Objects
+             */
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
     @Override
