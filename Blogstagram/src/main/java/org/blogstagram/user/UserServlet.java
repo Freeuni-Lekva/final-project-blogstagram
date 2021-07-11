c139785d065c78e761380dc4954c45fae95b8d23
auttenbergo <gigi.gvaramia@gmail.com>
2021-07-11 21:01:43 +0400

    Added raw frontend and outline for UserPageServlet

---
+++ Blogstagram/src/main/java/org/blogstagram/user/UserServlet.java
@@ -0,0 +1,47 @@
+package org.blogstagram.user;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Path;
+import java.io.IOException;
+
+
+@WebServlet("/user")
+public class UserServlet extends HttpServlet {
+
+    /*
+     * URL : /user/identificator
+     */
+    private String getPathIdentificator(HttpServletRequest req){
+        String pathInfo = req.getPathInfo();
+        String[] pathParts = pathInfo.split("/");
+        if(pathParts.length > 1)
+            return
+        return null;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String identificator = getPathIdentificator(req);
+        if(identificator == null){
+            /*
+                Logic
+             */
+            return;
+        }
+        boolean isID = identificator.matches("[0-9]+");
+        if(isID){
+            Integer userID = Integer.parseInt(identificator);
+            /*
+                Logic
+             */
+        }
+        /*
+            Logic if identificator is nickname
+         */
+
+    }
+}
---
+++ Blogstagram/src/main/webapp/jsp/user/userPage.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+    <head>
+        <title>Blogstagram</title>
+        <jsp:include page="/jsp/templates/bootstrap.jsp" />
+
+    </head>
+    <body>
+        <jsp:include page="/jsp/templates/nav.jsp" />
+
+
+
+        <jsp:include page="/jsp/templates/footer.jsp" />
+    </body>
+</html>
