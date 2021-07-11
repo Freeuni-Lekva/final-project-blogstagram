0a7fb55e80198d98eb40d3443e8c3356776e1a23
tdval19 <tdval19@freeuni.edu.ge>
2021-07-11 13:40:29 +0400

    login: validators update: validation for empty inputs, added validators in loginservlet

--- Blogstagram/src/main/java/org/blogstagram/login/LoginServlet.java
+++ Blogstagram/src/main/java/org/blogstagram/login/LoginServlet.java
@@ -1,14 +1,28 @@
 package org.blogstagram.login;
 
+import com.google.gson.Gson;
+import org.blogstagram.dao.UserDAO;
+import org.blogstagram.errors.GeneralError;
+import org.blogstagram.errors.VariableError;
+import org.blogstagram.models.User;
+import org.blogstagram.validators.LoginValidator;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
-
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+
+    private Connection getConnectionFromContext(HttpServletRequest req){
+        Connection connection = (Connection)req.getServletContext().getAttribute("dbConnection");
+        return connection;
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("/jsp/login/login.jsp");
@@ -17,6 +31,28 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Connection dbConnection = getConnectionFromContext(request);
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        LoginValidator validator = new LoginValidator(email, password, dbConnection);
+        try {
+            if(!validator.validate()) {
+                List<GeneralError> errors = validator.getErrors();
+                Gson gson = new Gson();
+                response.getWriter().print(gson.toJson(errors));
+                return;
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+////        UserDAO userDAO = (UserDAO) request.getSession().getAttribute("UserDAO");
+//        try {
+////            User user = userDAO.getUserByID(validator.getUserByEmail());
+//            request.getSession().setAttribute("currentUserID", user.getId());
+//            request.getSession().setAttribute("currentUserNickname", user.getNickname());
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
 
     }
 }
--- Blogstagram/src/main/java/org/blogstagram/validators/LoginValidator.java
+++ Blogstagram/src/main/java/org/blogstagram/validators/LoginValidator.java
@@ -1,27 +1,49 @@
 package org.blogstagram.validators;
 
 import org.blogstagram.errors.GeneralError;
+import org.blogstagram.errors.VariableError;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LoginValidator implements Validator {
 
     private final UserExistsValidator userExists;
-
+    private List<GeneralError> errors;
+    private final String email;
+    private final String password;
+    private static final String NOT_INCLUDED_EMAIL = "Email mustn't be empty";
+    private static final String NOT_INCLUDED_PASSWORD = "Password mustn't be empty";
 
     public LoginValidator(String email, String password, Connection connection) {
+        this.email = email;
+        this.password = password;
         userExists = new UserExistsValidator(connection, email, password);
     }
 
     @Override
     public boolean validate() throws SQLException {
-        return userExists.validate();
+        errors = new ArrayList<>();
+        if(email == null) {
+            errors.add(new VariableError("email", NOT_INCLUDED_EMAIL));
+        }
+        if(password == null) {
+            errors.add(new VariableError("password", NOT_INCLUDED_PASSWORD));
+        }
+        EmailFormatValidator emailFormatValidator = new EmailFormatValidator(email);
+        if(!emailFormatValidator.validate()){
+            errors.addAll(emailFormatValidator.getErrors());
+        }
+        if(!userExists.validate()) {
+            errors.addAll(userExists.getErrors());
+        }
+        return errors.size() == 0;
     }
 
     @Override
     public List<GeneralError> getErrors() {
-        return userExists.getErrors();
+        return errors;
     }
 }
