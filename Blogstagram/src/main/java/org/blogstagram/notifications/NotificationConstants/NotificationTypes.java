d63304aca9138d0945ff33c6fa6f28450564b1ab
tdval19 <tdval19@freeuni.edu.ge>
2021-08-15 18:48:13 +0400

    added Notification constants

---
+++ Blogstagram/src/main/java/org/blogstagram/notifications/NotificationConstants/NotificationTypes.java
@@ -0,0 +1,10 @@
+package org.blogstagram.notifications.NotificationConstants;
+
+public class NotificationTypes {
+    public static final int FOLLOW_NOTIFICATION = 0;
+    public static final int REQUESTED_FOLLOW_NOTIFICATION = 1;
+    public static final int ACCEPTED_FOLLOW_NOTIFICATION = 2;
+    public static final int POST_LIKE_NOTIFICATION = 3;
+    public static final int POST_COMMENT_NOTIFICATION = 4;
+    public static final int COMMENT_LIKE_NOTIFICATION = 5;
+}
